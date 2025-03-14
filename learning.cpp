ac54e77a7aaa5413ce81f2e9a1ad3b5ff43b342a
hunterterms <anaysahu@gmail.com>
2025-03-14 18:10:39 +0530

    test

---
+++ learning.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+int palindrome(){
+    int num = 121;
+    int reverse, rem;
+
+    while (num!=0) {
+        rem=num%10;
+        reverse = reverse*10 + rem;
+        num = num/10;
+    }
+
+    if (reverse==num){
+        cout << "true";
+
+    }
+    else{
+        cout <<"false";
+    }
+    return 0;
+
+}
+
+
+//void Vector() {
+  //  vector<int> v;
+    //v.push_back (1);
+    //v.emplace_back (1,2);
+//}
+
+int main() {
+    palindrome();
+    return 0;
+}
